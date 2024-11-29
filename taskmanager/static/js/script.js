5729fbd5a95b3b48295d6b4f50908adb47ec44b6
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-11-29 12:44:50 +0000

    Add add_task.html and post functionality in routes.py

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,6 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
 
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" }
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
   // delete model initialization
   let delModal = document.querySelectorAll('.modal');
   M.Modal.init(delModal);
