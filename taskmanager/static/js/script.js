79a6b4555e51dac505bf8d62494832d3ec7b45c9
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-11-29 07:21:03 +0000

    Implemented the lazy modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // delete model initialization
+  let delModal = document.querySelectorAll('.modal');
+  M.Modal.init(delModal);
+});
